6f97513c9dac0d444aebcbf9b93d4439f6e80a3b
rohan.sawahn <rohan.sawahn@student.hpi.de>
2023-06-02 12:43:16 +0200

    added code to run face landmark extraction, however producing empty results currently...

--- masking_tool/backend/helpers.py
+++ masking_tool/backend/helpers.py
@@ -1,5 +1,6 @@
 import os
 
+import mediapipe as mp
 import numpy as np
 import cv2
 
@@ -25,6 +26,46 @@ def draw_landmarks_on_image(rgb_image, detection_result):
         solutions.pose.POSE_CONNECTIONS,
         solutions.drawing_styles.get_default_pose_landmarks_style())
     return annotated_image
+
+def draw_landmarks_on_image_face(rgb_image, detection_result):
+  face_landmarks_list = detection_result.face_landmarks
+  annotated_image = np.copy(rgb_image)
+
+  # Loop through the detected faces to visualize.
+  for idx in range(len(face_landmarks_list)):
+    face_landmarks = face_landmarks_list[idx]
+
+    # Draw the face landmarks.
+    face_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+    face_landmarks_proto.landmark.extend([
+      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in face_landmarks
+    ])
+
+    solutions.drawing_utils.draw_landmarks(
+        image=annotated_image,
+        landmark_list=face_landmarks_proto,
+        connections=mp.solutions.face_mesh.FACEMESH_TESSELATION,
+        landmark_drawing_spec=None,
+        connection_drawing_spec=mp.solutions.drawing_styles
+        .get_default_face_mesh_tesselation_style())
+    solutions.drawing_utils.draw_landmarks(
+        image=annotated_image,
+        landmark_list=face_landmarks_proto,
+        connections=mp.solutions.face_mesh.FACEMESH_CONTOURS,
+        landmark_drawing_spec=None,
+        connection_drawing_spec=mp.solutions.drawing_styles
+        .get_default_face_mesh_contours_style())
+    solutions.drawing_utils.draw_landmarks(
+        image=annotated_image,
+        landmark_list=face_landmarks_proto,
+        connections=mp.solutions.face_mesh.FACEMESH_IRISES,
+          landmark_drawing_spec=None,
+          connection_drawing_spec=mp.solutions.drawing_styles
+          .get_default_face_mesh_iris_connections_style())
+
+    cv2.imshow("img", cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
+    return annotated_image
+
 
 def create_black_bg(video_path: str) -> str:
     cap = cv2.VideoCapture(video_path)
--- masking_tool/backend/masking.py
+++ masking_tool/backend/masking.py
@@ -9,7 +9,7 @@ from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
 from ultralytics import YOLO
 
-from helpers import draw_landmarks_on_image
+from helpers import draw_landmarks_on_image, draw_landmarks_on_image_face
 
 
 def blur(video_path: str, background_video_path: str, part_to_blur: Literal["body", "face"], confidence_treshold: float) -> str:
@@ -47,6 +47,7 @@ def blur(video_path: str, background_video_path: str, part_to_blur: Literal["bod
 
     out.release()
     video_cap.release()
+    return vid_out_path
 
 def extract_skeleton(video_path: str, background_video_path: str, framework: Literal["mediapipe"]) -> str:
     model_path = os.path.join("models", "pose_landmarker_lite.task")
@@ -112,6 +113,71 @@ def extract_skeleton(video_path: str, background_video_path: str, framework: Lit
         capture.release()
         capture_bg.release()
         cv2.destroyAllWindows()
+    return output_path
 
 def extract_face(video_path: str, background_video_path: str, framework: Literal["mediapipe"]) -> str:
-    pass
+    model_path = os.path.join("models", "face_landmarker.task")
+
+    BaseOptions = mp.tasks.BaseOptions
+    FaceLandmarker = mp.tasks.vision.FaceLandmarker
+    FaceLandmarkerOptions = mp.tasks.vision.FaceLandmarkerOptions
+    VisionRunningMode = mp.tasks.vision.RunningMode
+
+    options = FaceLandmarkerOptions(
+        base_options=BaseOptions(model_asset_path=model_path),
+        running_mode=VisionRunningMode.VIDEO,
+        output_face_blendshapes=True,
+        output_facial_transformation_matrixes=True,
+        num_faces=1)
+
+
+    with FaceLandmarker.create_from_options(options) as landmarker:
+        output_path = os.path.join("results", os.path.split(video_path)[1])
+        capture = cv2.VideoCapture(video_path)
+
+        frameWidth = capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+        frameHeight = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        samplerate = capture.get(cv2.CAP_PROP_FPS)
+        capture = cv2.VideoCapture(video_path)
+        capture_bg = cv2.VideoCapture(background_video_path)
+
+        if int(capture.get(cv2.CAP_PROP_FRAME_COUNT)) != int(capture_bg.get(cv2.CAP_PROP_FRAME_COUNT)):
+            raise Exception("Background Video not same length as Video to mask")
+
+        fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+        out = cv2.VideoWriter(output_path, fourcc, fps = samplerate, frameSize = (int(frameWidth), int(frameHeight)))
+
+        first = True
+        while capture.isOpened():
+            ret, frame = capture.read()
+            _, frame_bg = capture_bg.read()
+
+            if ret:
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                frame_timestamp_ms = capture.get(cv2.CAP_PROP_POS_MSEC)
+
+                mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+                mp_image_bg = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame_bg)
+
+                if not first and frame_timestamp_ms == 0: # Fallback for videos with bad codec
+                    print("Bad coded probably - can't properly processsome frames")
+                    continue
+
+                face_landmarker_result = landmarker.detect_for_video(mp_image, int(frame_timestamp_ms))
+                print(face_landmarker_result)
+
+                output_image = cv2.cvtColor(mp_image_bg.numpy_view(), cv2.COLOR_RGB2BGR)
+                output_image = draw_landmarks_on_image_face(frame_bg, face_landmarker_result)
+
+                out.write(output_image)
+                first = False
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+            else:
+                break
+
+        out.release()
+        capture.release()
+        capture_bg.release()
+        cv2.destroyAllWindows()
+    return output_path
