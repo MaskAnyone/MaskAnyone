420f4793e20fe0852961fe5b7b218d59dee4cc42
Martin Schilling <martin.schilling@insecia.com>
2023-06-02 11:55:29 +0200

    Revert unintended changes and add mask button

--- masking_tool/frontend/src/components/videos/VideosPage.tsx
+++ masking_tool/frontend/src/components/videos/VideosPage.tsx
@@ -3,8 +3,9 @@ import Api from "../../api";
 import {useDispatch} from "react-redux";
 import Event from "../../state/actions/event";
 import {useParams} from "react-router";
-import {Box} from "@mui/material";
+import {Box, Button} from "@mui/material";
 import Config from "../../config";
+import MasksIcon from '@mui/icons-material/Masks';
 
 const VideosPage = () => {
     const dispatch = useDispatch();
@@ -18,6 +19,9 @@ const VideosPage = () => {
 
     return (
         <Box>
+            <Box sx={{ marginBottom: 2 }}>
+                <Button variant={'contained'} startIcon={<MasksIcon />}>Mask Video</Button>
+            </Box>
             {videoName && (
                 <video controls={true} key={videoName} style={{ maxWidth: '100%' }}>
                     <source src={Config.api.baseUrl + '/videos/' + videoName} type={'video/mp4'} key={videoName} />
