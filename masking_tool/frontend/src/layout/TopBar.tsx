acecf0d7b1ea3bfcb82b90e233307e0c7eb89c7b
rohan.sawahn <rohan.sawahn@student.hpi.de>
2023-06-24 12:46:14 +0200

    added runs-page with overview of runs at their corresponding status

--- masking_tool/frontend/src/layout/TopBar.tsx
+++ masking_tool/frontend/src/layout/TopBar.tsx
@@ -1,5 +1,7 @@
-import {AppBar, IconButton, Toolbar} from "@mui/material";
+import {AppBar, Box, Button, IconButton, Toolbar, Typography} from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from "react-router-dom";
+import MaskIcon from '@mui/icons-material/Masks';
 
 const styles = {
     appBar: (theme: any) => ({
@@ -18,12 +20,44 @@ interface TopBarProps {
     onOpenSideBar?: () => void;
 }
 
+const pages = [{name: 'Runs', url: '/runs'}, {name: 'Presets', url: '/presets'}]
+
 const TopBar = (props: TopBarProps) => {
     return (
         <AppBar position={'fixed'} color={'primary'} sx={styles.appBar}>
             <Toolbar sx={styles.toolbar}>
+            <MaskIcon sx={{ display: { xs: 'flex', md: 'flex' }, mr: 1 }} />
+            <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="/"
+                sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.12rem',
+                color: 'inherit',
+                textDecoration: 'none',
+                }}
+            >
+                MaskAnyone
+            </Typography>
+                <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'flex' } }}>
+                    {pages.map((page) => (
+                    <Button
+                        key={page.name}
+                        sx={{ my: 2, color: 'white', display: 'block' }}
+                        component={Link}
+                        to={page.url}
+                    >
+                        {page.name}
+                    </Button>
+                    ))}
+                </Box>
                 {(!props.isLargeScreen) && (
-                    <IconButton color={'secondary'} onClick={props.onOpenSideBar}>
+                    <IconButton sx={{color: 'white'}} onClick={props.onOpenSideBar}>
                         <MenuIcon />
                     </IconButton>
                 )}
---
+++ masking_tool/frontend/src/pages/RunsPage.tsx
@@ -0,0 +1,301 @@
+import * as React from 'react';
+import { alpha } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+import DeleteIcon from '@mui/icons-material/Delete';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import { visuallyHidden } from '@mui/utils';
+import { Chip } from '@mui/material';
+
+interface Data {
+  id: string
+  videoName: string
+  params: string
+  duration: number
+  status: string
+}
+
+function createData(
+  id: string,
+  videoName: string,
+  params: string,
+  duration: number,
+  status: string
+): Data {
+  return {
+    id,
+    videoName,
+    params,
+    duration,
+    status,
+  }
+}
+
+const statusColors: { [status: string] : "info"|"success"|"error" } = {
+    "running": "info",
+    "success": "success",
+    "failed": "error"
+}
+
+const rows = [
+  createData('run1', 'ted.mp4',"bbox, mediapipe", 23, "running"),
+  createData('run2', 'ted.mp4',"bbox, mediapipe", 230, "success"),
+  createData('run3', 'ted.mp4',"bbox, mediapipe", 231, "failed"),
+  createData('run4', 'ted.mp4',"bbox, mediapipe", 203, "success"),
+  createData('run5', 'ted.mp4',"bbox, mediapipe", 310, "success"),
+  createData('run6', 'ted.mp4',"bbox, mediapipe", 23, "success"),
+  createData('run7', 'ted.mp4',"bbox, mediapipe", 23, "success"),
+];
+
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+type Order = 'asc' | 'desc';
+
+function getComparator<Key extends keyof any>(
+  order: Order,
+  orderBy: Key,
+): (
+  a: { [key in Key]: number | string },
+  b: { [key in Key]: number | string },
+) => number {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+// Since 2020 all major browsers ensure sort stability with Array.prototype.sort().
+// stableSort() brings sort stability to non-modern browsers (notably IE11). If you
+// only support modern browsers you can replace stableSort(exampleArray, exampleComparator)
+// with exampleArray.slice().sort(exampleComparator)
+function stableSort<T>(array: readonly T[], comparator: (a: T, b: T) => number) {
+  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+interface HeadCell {
+  disablePadding: boolean;
+  id: keyof Data;
+  label: string;
+  numeric: boolean;
+}
+
+const headCells: readonly HeadCell[] = [
+  {
+    id: 'id',
+    numeric: false,
+    disablePadding: true,
+    label: 'ID',
+  },
+  {
+    id: 'videoName',
+    numeric: true,
+    disablePadding: false,
+    label: 'Video Name',
+  },
+  {
+    id: 'params',
+    numeric: true,
+    disablePadding: false,
+    label: 'Params',
+  },
+  {
+    id: 'duration',
+    numeric: true,
+    disablePadding: false,
+    label: 'Duration',
+  },
+  {
+    id: 'status',
+    numeric: true,
+    disablePadding: false,
+    label: 'Status',
+  },
+];
+
+interface EnhancedTableProps {
+  onRequestSort: (event: React.MouseEvent<unknown>, property: keyof Data) => void;
+  order: Order;
+  orderBy: string;
+  rowCount: number;
+}
+
+function EnhancedTableHead(props: EnhancedTableProps) {
+  const { order, orderBy, rowCount, onRequestSort } =
+    props;
+  const createSortHandler =
+    (property: keyof Data) => (event: React.MouseEvent<unknown>) => {
+      onRequestSort(event, property);
+    };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'normal'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+const RunsPage = () => {
+  const [order, setOrder] = React.useState<Order>('asc');
+  const [orderBy, setOrderBy] = React.useState<keyof Data>('id');
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleRequestSort = (
+    event: React.MouseEvent<unknown>,
+    property: keyof Data,
+  ) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const visibleRows = React.useMemo(
+    () =>
+      stableSort(rows, getComparator(order, orderBy)).slice(
+        page * rowsPerPage,
+        page * rowsPerPage + rowsPerPage,
+      ),
+    [order, orderBy, page, rowsPerPage],
+  );
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Paper sx={{ width: '100%', mb: 2 }}>
+        <TableContainer sx={{padding: "20px", minHeight: "80vh"}}>
+        <Typography
+          sx={{ flex: '1 1 100%' }}
+          variant="h6"
+          id="tableTitle"
+          component="div"
+        >
+          Masking Runs
+        </Typography>
+          <Table
+            sx={{ minWidth: 750 }}
+            aria-labelledby="runsTable"
+            size={'medium'}
+          >
+            <EnhancedTableHead
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={handleRequestSort}
+              rowCount={rows.length}
+            />
+            <TableBody>
+              {visibleRows.map((row, index) => {
+
+                return (
+                  <TableRow
+                    hover
+                    tabIndex={-1}
+                    key={row.id}
+                    sx={{ cursor: 'pointer' }}
+                  >
+                    <TableCell
+                      component="th"
+                      id={String(index)}
+                      scope="row"
+                      padding="none"
+                    >
+                      {row.id}
+                    </TableCell>
+                    <TableCell align="right">{row.videoName}</TableCell>
+                    <TableCell align="right">{row.params}</TableCell>
+                    <TableCell align="right">{row.duration}</TableCell>
+                    <TableCell align="right">
+                        <Chip label={row.status} color={statusColors[row.status]} />
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+              {emptyRows > 0 && (
+                <TableRow
+                  style={{
+                    height: (53) * emptyRows,
+                  }}
+                >
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </Box>
+  );
+}
+
+export default RunsPage
