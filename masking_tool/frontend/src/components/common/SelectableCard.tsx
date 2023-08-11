22f3489b9d70ff1ab10ecf601416cb02b819ac58
Martin Schilling <martin.schilling@insecia.com>
2023-08-11 13:55:31 +0200

    make presets look nice

--- masking_tool/frontend/src/components/common/SelectableCard.tsx
+++ masking_tool/frontend/src/components/common/SelectableCard.tsx
@@ -3,17 +3,25 @@ import React from "react";
 import SideBySideCard from "./SideBySideCard";
 import {ButtonBase} from "@mui/material";
 
+const styles = {
+    container: {
+        textAlign: 'inherit',
+        whiteSpace: 'initial',
+    },
+};
+
 interface SelectableCardProps {
     title: string;
     description: string;
     selected: boolean;
     imagePath: string;
     onSelect: () => void;
+    style?: React.CSSProperties;
 }
 
 const SelectableCard = (props: SelectableCardProps) => {
     return (
-        <ButtonBase onClick={props.onSelect} sx={{ textAlign: 'inherit' }}>
+        <ButtonBase onClick={props.onSelect} sx={styles.container} style={props.style}>
             <SideBySideCard image={props.imagePath}>
                 <SideBySideCardContent
                     title={props.title}
