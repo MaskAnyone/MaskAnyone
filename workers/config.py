a0dc452340e0e8df5124909061de823798f55ebb
Martin Schilling <martin.schilling@insecia.com>
2024-02-25 21:08:52 +0100

    drop all old worker-related files

--- workers/config.py
+++
@@ -1,9 +0,0 @@
-RESULT_BASE_PATH = "/local_data/results"
-VIDEOS_BASE_PATH = "/local_data/original"
-TS_BASE_PATH = "/local_data/timeseries"
-BLENDSHAPES_BASE_PATH = "/local_data/blendshapes"
-TEMP_PATH = "/local_data/temp"
-IMPLEMENTED_VIDEO_PARTS = ["body", "face", "background"]
-DATA_BASE_DIR = "/local_data"
-DOCKER_MODELS_CONFIG_PATH = "/app/docker_worker/configs"
-AVAILABLE_DOCKER_MODELS = ["roop", "blender"]
--- workers/pipeline_worker/pipeline/audio_masking/BaseAudioMasker.py
+++
@@ -1,6 +0,0 @@
-class BaseAudioMasker:
-    def __init__(self, params: dict):
-        pass
-
-    def mask(input_path):
-        pass
