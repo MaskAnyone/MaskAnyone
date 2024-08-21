be06a7fe309012fa292b2c4c8260435882866391
Martin Schilling <martin.schilling@insecia.com>
2024-08-21 21:29:16 +0200

    fix minor issues

--- worker/masking/sam2_pose_masker.py
+++ worker/masking/sam2_pose_masker.py
@@ -13,15 +13,6 @@ from mediapipe.framework.formats import landmark_pb2
 from masking.smoothing import smooth_pose
 from masking.mask_renderer import MaskRenderer
 
-colors = [
-    (0, 0, 255),   # Red
-    (0, 255, 0),   # Green
-    (255, 0, 0),   # Blue
-    (0, 255, 255), # Yellow
-    (255, 0, 255), # Magenta
-    (255, 255, 0), # Cyan
-    # Add more colors if needed
-]
 
 DEBUG = True
 SMOOTHING = True
@@ -238,8 +229,8 @@ def render_body25_pose(image, full_pose):
 
 MP_POSE_PAIRS = [
     (0, 1), (1, 2), (2, 3), (3, 7), (0, 4), (4, 5), (5, 6), (6, 8),
-    (9, 10), (11, 12), (11, 13), (13, 15), (15, 19), (15, 17), (17, 19),
-    (12, 14), (14, 16), (16, 20), (16, 18), (18, 20), (11, 23), (12, 24),
+    (9, 10), (11, 12), (11, 13), (13, 15), (15, 19), (15, 17), (17, 19), (15, 21),
+    (12, 14), (14, 16), (16, 20), (16, 18), (18, 20), (11, 23), (12, 24), (16, 22),
     (23, 25), (24, 26), (25, 27), (26, 28), (23, 24),
     (28, 30), (28, 32), (30, 32), (27, 29), (27, 31), (29, 31)
 ]
