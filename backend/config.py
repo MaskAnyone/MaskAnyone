4eaee25b821a86980b2980e54873af16da4c613f
Martin Schilling <martin.schilling@student.hpi.de>
2024-12-12 15:59:06 +0100

    Add option to set keycloak issuer via environment

--- backend/config.py
+++ backend/config.py
@@ -4,7 +4,7 @@ RESULT_BASE_PATH = "/var/lib/maskanyone/data/results"
 VIDEOS_BASE_PATH = "/var/lib/maskanyone/data/videos"
 PRESETS_BASE_PATH = "/var/lib/maskanyone/data/presets"
 
-AUTH_TOKEN_ISSUER = "https://localhost/auth/realms/maskanyone"
+AUTH_TOKEN_ISSUER = os.getenv("BACKEND_AUTH_ISSUER", "https://localhost/auth/realms/maskanyone")
 AUTH_TOKEN_AUDIENCE = "account"
 AUTH_ALGORITHM = "RS256"
 AUTH_PUBLIC_KEY = "-----BEGIN PUBLIC KEY-----\n" + os.environ['BACKEND_AUTH_PUBLIC_KEY'] + "\n-----END PUBLIC KEY-----"
